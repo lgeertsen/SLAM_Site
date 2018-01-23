427734c9d5192c1e8ee3270b5c6d7c1f593138e1
Lee Geertsen <lee.geertsen@hotmail.com>
2018-01-23 17:28:27 +0100

    Add mysql db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'lets_go'),
+            'username' => env('DB_USERNAME', 'lets_go'),
+            'password' => env('DB_PASSWORD', 'jocelyn'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
