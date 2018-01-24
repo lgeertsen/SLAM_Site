c3ee8c1ecadfe3a43364b6684e04bc0cdfb816ce
Lee Geertsen <lee.geertsen@hotmail.com>
2018-01-24 12:58:19 +0100

    Add API links

---
+++ app/Http/Controllers/APIController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tournament;
+use App\Http\Resources\Tournament as TournamentResource;
+use App\Http\Resources\TournamentCollection;
+
+
+use Illuminate\Http\Request;
+
+class APIController extends Controller {
+  public function tournaments() {
+    return new TournamentCollection(Tournament::all());
+  }
+
+  public function tournament($id) {
+    TournamentResource::withoutWrapping();
+
+    return new TournamentResource(Tournament::find($id));
+  }
+}
