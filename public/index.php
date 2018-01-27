8818627035acdcf4574941a4d62a227ca89ac462
Lee Geertsen <lee.geertsen@hotmail.com>
2018-01-27 17:30:51 +0100

    Change header

--- public/index.php
+++ public/index.php
@@ -10,6 +10,9 @@
 define('LARAVEL_START', microtime(true));
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS");
+header("Access-Control-Allow-Credentials: true");
 
 /*
 |--------------------------------------------------------------------------
